b4f010ebae602f853c97c4091d74476d0399ccd9
y_okada <yasokada@gmail.com>
2015-09-05 22:58:13 +0900

    find which Toggle is set true, which is set false

---
+++ Assets/PanelDisplayControl.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class PanelDisplayControl : MonoBehaviour {
+
+	public GameObject PageSelect;
+	
+	public void ToggleValueChanged(int idx_st1) {
+		Toggle selected = null;
+		string name;
+		foreach (Transform child in PageSelect.transform) {
+			name = child.gameObject.name;
+			if (name.Contains(idx_st1.ToString())) { // e.g. "SelP1", "SelP2"...
+				selected = child.gameObject.GetComponent(typeof(Toggle)) as Toggle;
+				break;
+			}
+		}
+
+		Debug.Log(idx_st1.ToString() + " is " + selected.isOn);
+	}
+
+}
