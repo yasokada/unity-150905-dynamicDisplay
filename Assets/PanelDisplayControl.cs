f72a7bf306d89c9d827899e7518d29438c276a80
y_okada <yasokada@gmail.com>
2015-09-06 10:09:26 +0900

    use .localScale instead of .sizeDelta to remove orgSize[]

--- Assets/PanelDisplayControl.cs
+++ Assets/PanelDisplayControl.cs
@@ -3,6 +3,8 @@ using System.Collections;
 using UnityEngine.UI;
 
 /*
+ * v0.4 2015/09/06
+ *   - use localScale instead of keeping original sizez at orgSize[]
  * v0.3 2015/09/06
  *   - use Tag for Panel to find target panels, instead of panel name
  * v0.2 2015/09/06
@@ -15,23 +17,15 @@ public class PanelDisplayControl : MonoBehaviour {
 
 	public GameObject PageSelect;
 	public GameObject PanelGroup;
-
-	private Vector2[] orgSize;
-
+	
 	void Start() {
-		orgSize = new Vector2[4];
-		int idx=0;
-
+		int idx = 0;
 		foreach (Transform child in PanelGroup.transform) {
-			orgSize[idx].x = child.gameObject.GetComponent<RectTransform>().rect.width;
-			orgSize[idx].y = child.gameObject.GetComponent<RectTransform>().rect.height;
-
-			bool isOn = getIsOn (idx + 1);
-			DisplayPanel (idx + 1, isOn);
+			bool isOn = getIsOn (/* idx_st1= */ idx + 1);
+			DisplayPanel (/* idx_st1= */ idx + 1, isOn);
 
 			idx++;
 		}
-
 	}
 
 	bool getIsOn(int idx_st1) {
@@ -57,15 +51,15 @@ public class PanelDisplayControl : MonoBehaviour {
 
 		foreach (GameObject panel in panels) {
 			RectTransform rect = panel.GetComponent (typeof(RectTransform)) as RectTransform;
-			Vector2 size = rect.sizeDelta;
+			Vector2 size = rect.localScale;
 			if (doShow) {
-				size.x = orgSize [idx_st1 - 1].x;
-				size.y = orgSize [idx_st1 - 1].y;
+				size.x = 1.0f;
+				size.y = 1.0f;
 			} else {
 				size.x = 0;
 				size.y = 0;
 			}
-			rect.sizeDelta = size;
+			rect.localScale = size;
 		}
 	}
 
